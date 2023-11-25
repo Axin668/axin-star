1b24d024476918cf2dcdda07e34c99380250e600
axin668 <axin99966@163.com>
2023-11-25 22:41:39 +0800

    feat: 增加管理员所有的接口以及类型(VO, Form等)定义

--- src/api/manager/index.ts
+++ src/api/manager/index.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 import { AxiosPromise } from 'axios'
-import { ManagerInfo } from './types'
+import { ManagerForm, ManagerInfo, ManagerPageVO, ManagerQuery } from './types'
 
 /**
  * 登录成功后获取用户信息（昵称、头像、权限集合和角色集合）
@@ -12,3 +12,145 @@ export function getManagerInfo(): AxiosPromise<ManagerInfo> {
   })
 }
 
+/**
+ * 获取管理员分页列表
+ * 
+ * @param queryParams 
+ */
+export function getManagerPage(
+  queryParams: ManagerQuery
+): AxiosPromise<global.PageResult<ManagerPageVO[]>> {
+  return request({
+    url: "/api/managers/page",
+    method: "get",
+    params: queryParams
+  })
+}
+
+/**
+ * 获取管理员表单详情
+ * 
+ * @param managerId 
+ */
+export function getManagerForm(managerId: number): AxiosPromise<ManagerForm> {
+  return request({
+    url: "/api/managers/" + managerId + "/form",
+    method: "get",
+  });
+}
+
+/**
+ * 添加管理员
+ * 
+ * @param data 
+ */
+export function addManager(data: any) {
+  return request({
+    url: "/api/managers",
+    method: "post",
+    data: data
+  });
+}
+
+/**
+ * 修改管理员信息
+ * 
+ * @param id 
+ * @param data 
+ */
+export function updateManager(id: number, data: ManagerForm) {
+  return request({
+    url: "/api/managers/" + id,
+    method: "put",
+    data: data,
+  });
+}
+
+/**
+ * 修改管理员状态
+ * 
+ * @param id 
+ * @param status 
+ */
+export function updateManagerStatus(id: number, status: number) {
+  return request({
+    url: "/api/managers/" + id + "/status",
+    method: "patch",
+    params: { status: status }
+  });
+}
+
+/**
+ * 修改管理员密码
+ * 
+ * @param id 
+ * @param password 
+ */
+export function updateManagerPassword(id: number, password: string) {
+  return request({
+    url: "/api/managers/" + id + "/password",
+    method: "patch",
+    params: { password: password },
+  });
+}
+
+/**
+ * 批量删除管理员
+ * 
+ * @param ids 
+ */
+export function deleteManagers(ids: string) {
+  return request({
+    url: "/api/managers/" + ids,
+    method: "delete",
+  });
+}
+
+/**
+ * 下载管理员导入模板
+ * 
+ * @returns
+ */
+export function downloadTemplateApi() {
+  return request({
+    url: "/api/managers/template",
+    method: "get",
+    responseType: "arraybuffer",
+  });
+}
+
+/**
+ * 导出管理员
+ * 
+ * @param queryParams 
+ * @returns 
+ */
+export function exportManager(queryParams: ManagerQuery) {
+  return request({
+    url: "/api/managers/_export",
+    method: "get",
+    params: queryParams,
+    responseType: "arraybuffer",
+  });
+}
+
+/**
+ * 导入管理员
+ * 
+ * @param deptId 
+ * @param file 
+ * @returns 
+ */
+export function importManager(deptId: number, file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+  return request({
+    url: "/api/managers/_import",
+    method: "post",
+    params: { deptId: deptId },
+    data: formData,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    }
+  })
+}
--- src/api/manager/types.ts
+++ src/api/manager/types.ts
@@ -1,5 +1,5 @@
 /**
- * 登录用户信息
+ * 管理员登录信息
  */
 export interface ManagerInfo {
   managerId?: number
@@ -9,3 +9,99 @@ export interface ManagerInfo {
   perms: string[]
 }
 
+/**
+ * 管理员查询对象类型
+ */
+export interface ManagerQuery extends global.PageQuery {
+  keywords?: string;
+  status?: number;
+  deptId?: number;
+}
+
+/**
+ * 管理员分页对象
+ */
+export interface ManagerPageVO {
+  /**
+   * 管理员头像地址
+   */
+  avatar?: string;
+  /**
+   * 创建时间
+   */
+  createTime?: Date;
+  /**
+   * 部门名称
+   */
+  deptName?: string;
+  /**
+   * 管理员邮箱
+   */
+  email?: string;
+  /**
+   * 性别
+   */
+  genderLabel?: string;
+  /**
+   * 管理员ID
+   */
+  id?: number;
+  /**
+   * 管理员手机号
+   */
+  mobile?: number;
+  /**
+   * 管理员拥有的角色名称, 多个使用英文逗号(,)分割
+   */
+  roleNames?: string;
+  /**
+   * 管理员状态(1:启用, 0:禁用)
+   */
+  status?: number;
+  /**
+   * 管理员名称
+   */
+  managerName?: string;
+}
+
+/**
+ * 管理员表单类型
+ */
+export interface ManagerForm {
+  /**
+   * 管理员头像
+   */
+  avatar?: string;
+  /**
+   * 部门ID
+   */
+  deptId?: number;
+  /**
+   * 邮箱
+   */
+  email?: string;
+  /**
+   * 性别
+   */
+  gender?: number;
+  /**
+   * 管理员ID
+   */
+  id?: number;
+  /**
+   * 手机号
+   */
+  mobile?: string;
+  /**
+  * 角色ID集合
+  */
+  roleIds: number[];
+  /**
+  * 管理员状态(1:正常, 0:禁用)
+  */
+  status?: number;
+  /**
+   * 管理员名称
+   */
+  managerName?: string;
+}
